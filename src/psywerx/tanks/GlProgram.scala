b39da518b0108612fa527fab3be57c96f77c4f37
Smotko <ensmotko@gmail.com>
2012-12-08 19:21:13 +0100

    Added imports to GlProgram

--- src/psywerx/tanks/GlProgram.scala
+++ src/psywerx/tanks/GlProgram.scala
@@ -1,13 +1,4 @@
 package psywerx.tanks
-
-import javax.media.opengl.GL.GL_RGBA
-import javax.media.opengl.GL.GL_TEXTURE_2D
-import javax.media.opengl.GL.GL_TEXTURE_MAG_FILTER
-import javax.media.opengl.GL.GL_TEXTURE_MIN_FILTER
-import javax.media.opengl.GL.GL_UNPACK_ALIGNMENT
-import javax.media.opengl.GL.GL_UNSIGNED_BYTE
-import javax.media.opengl.GL.GL_LINEAR
-import javax.media.opengl.GL.GL_LINEAR_MIPMAP_NEAREST
 import javax.media.opengl.{GL2ES2 => GL}
 import javax.imageio.ImageIO
 import java.nio.ByteBuffer
@@ -15,23 +6,26 @@ import java.awt.image.Raster
 
 class GlProgram (val gl:GL) {
   
-  val program = gl.glCreateProgram()
+  import gl._
+  import GL._
   
-  private val vertShader = gl.glCreateShader(GL.GL_VERTEX_SHADER)
-  private val fragShader = gl.glCreateShader(GL.GL_FRAGMENT_SHADER)
+  val program = glCreateProgram()
+  
+  private val vertShader = glCreateShader(GL_VERTEX_SHADER)
+  private val fragShader = glCreateShader(GL_FRAGMENT_SHADER)
   
   createShader(vertShader, Shaders.vertex)
   createShader(fragShader, Shaders.fragment)
   link()
   
-  val positionLoc = gl.glGetAttribLocation(program, "attribute_Position")
-  val colorLoc    = gl.glGetAttribLocation(program, "attribute_Color")
-  val texLoc      = gl.glGetAttribLocation(program, "a_texCoord")
+  val positionLoc = glGetAttribLocation(program, "attribute_Position")
+  val colorLoc    = glGetAttribLocation(program, "attribute_Color")
+  val texLoc      = glGetAttribLocation(program, "a_texCoord")
 
-  val projectionMatrixLoc = gl.glGetUniformLocation(program, "uniform_Projection")
-  val modelMatrixLoc      = gl.glGetUniformLocation(program, "uniform_Model")
-  val samplerLoc          = gl.glGetUniformLocation(program, "s_texture")
-  val isTextLoc           = gl.glGetUniformLocation(program, "u_isText")
+  val projectionMatrixLoc = glGetUniformLocation(program, "uniform_Projection")
+  val modelMatrixLoc      = glGetUniformLocation(program, "uniform_Model")
+  val samplerLoc          = glGetUniformLocation(program, "s_texture")
+  val isTextLoc           = glGetUniformLocation(program, "u_isText")
   val texture = loadTexture("/res/text.png")
   
   
@@ -46,47 +40,49 @@ class GlProgram (val gl:GL) {
     val pixels = data.getPixels(0,0,image.getWidth(), image.getHeight(),t)
     val pixelBuffer = ByteBuffer.wrap(pixels.map { p => p.asInstanceOf[Byte] })
     
-    gl.glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-    gl.glGenTextures(1, texture, 0)
-    gl.glBindTexture(GL_TEXTURE_2D, texture(0))
-    gl.glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.getHeight(), image.getWidth(), 0, GL_RGBA, GL_UNSIGNED_BYTE, pixelBuffer)
-    gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST)
-    gl.glGenerateMipmap(GL_TEXTURE_2D)
+    import javax.media.opengl.GL._
+    
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+    glGenTextures(1, texture, 0)
+    glBindTexture(GL_TEXTURE_2D, texture(0))
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.getHeight(), image.getWidth(), 0, GL_RGBA, GL_UNSIGNED_BYTE, pixelBuffer)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST)
+    glGenerateMipmap(GL_TEXTURE_2D)
     
     texture(0)
   }
   
   private def createShader(shader:Int, source:String) = {
     val srcArr = Array(source)
-    gl.glShaderSource(shader, srcArr.length, srcArr, Array(source.length()), 0)
-    gl.glCompileShader(shader)
+    glShaderSource(shader, srcArr.length, srcArr, Array(source.length()), 0)
+    glCompileShader(shader)
     
     var compiled = Array[Int](0)
-    gl.glGetShaderiv(shader, GL.GL_COMPILE_STATUS, compiled, 0)
+    glGetShaderiv(shader, GL_COMPILE_STATUS, compiled, 0)
     if (compiled(0) == 0){
       var logLength = Array[Int](0);
-      gl.glGetShaderiv(shader, GL.GL_INFO_LOG_LENGTH, logLength, 0)
+      glGetShaderiv(shader, GL_INFO_LOG_LENGTH, logLength, 0)
         
       var log = Array[Byte](0);
-      gl.glGetShaderInfoLog(shader, logLength(0), null, 0, log, 0)
+      glGetShaderInfoLog(shader, logLength(0), null, 0, log, 0)
         
         println("Error compiling the shader: " + new String(log))
         System.exit(1);
     }
-    gl.glAttachShader(program, shader)
+    glAttachShader(program, shader)
   }
   def link() = {
-    gl.glLinkProgram(program)
+    glLinkProgram(program)
   }
   
   def dispose() = {
-    gl.glUseProgram(program)
-    gl.glDetachShader(program, vertShader)
-    gl.glDeleteShader(vertShader)
-    gl.glDetachShader(program, fragShader)
-    gl.glDeleteShader(fragShader)
-    gl.glDeleteProgram(program)
+    glUseProgram(program)
+    glDetachShader(program, vertShader)
+    glDeleteShader(vertShader)
+    glDetachShader(program, fragShader)
+    glDeleteShader(fragShader)
+    glDeleteProgram(program)
   }
   
   
